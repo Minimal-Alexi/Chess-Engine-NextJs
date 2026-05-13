fe35e41cdb9b35f91bbf025ac799156b085f9368
Minimal-Alexi <alexpop@metropolia.fi>
2026-05-13 11:39:27 +0300

    Tested out footer.

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <div>
+            <h4>Minimal Chess Engine Trademarked and all rights reserved</h4>
+        </div>
+    )
+}
