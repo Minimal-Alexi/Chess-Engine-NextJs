02802212f223d63b3c67fde2c5e49db6a03ceefe
Minimal-Alexi <alexpop@metropolia.fi>
2026-05-14 12:47:04 +0300

    Displaying images on the board.

--- components/chess/Tile.tsx
+++ components/chess/Tile.tsx
@@ -1,5 +1,25 @@
+const pieceToImage: Record<string, string> = {
+  K: "/pieces/wk.png",
+  Q: "/pieces/wq.png",
+  R: "/pieces/wr.png",
+  B: "/pieces/wb.png",
+  N: "/pieces/wn.png",
+  P: "/pieces/wp.png",
+
+  k: "/pieces/bk.png",
+  q: "/pieces/bq.png",
+  r: "/pieces/br.png",
+  b: "/pieces/bb.png",
+  n: "/pieces/bn.png",
+  p: "/pieces/bp.png",
+};
+
+import Image from "next/image";
+
 export const RenderSquare = (row:number, col:number, piece: string) => {
     const isDark = (row + col) % 2 === 1;
+    const isPiece = piece != ' '
+    
 
     return (
       <div
@@ -9,7 +29,14 @@ export const RenderSquare = (row:number, col:number, piece: string) => {
           height: 100,
           backgroundColor: isDark ? "#93452a" : "b28a7c",
         }}>
-            {piece !== " " ? piece : null}
+                  {piece && piece !== " " && (
+        <Image
+          src={pieceToImage[piece]}
+          alt={piece}
+          width={100}
+          height={100}
+        />
+      )}
         </div>
     );
   };
