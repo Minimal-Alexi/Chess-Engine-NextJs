e508c1d55be14f0222a19d5643acf3a54c8acd36
Minimal-Alexi <alexpop@metropolia.fi>
2026-05-14 11:44:33 +0300

    Added todo for license of project.

---
+++ components/chess/Board.tsx
@@ -0,0 +1,9 @@
+// TODO: Mark github projects under CC BY-SA 3.0 license. Credit to: Cburnett (from wikipedia) for pieces.
+
+export const Board = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
