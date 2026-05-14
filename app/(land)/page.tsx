765ac914be00f8d8a0b324011d78fc02cce1fb2d
Minimal-Alexi <alexpop@metropolia.fi>
2026-05-14 08:41:39 +0300

    Made a home page.

--- app/(land)/page.tsx
+++ app/(land)/page.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>Hello world</main>
+    <main>
+      <div className="container mx-auto m-8 p-4 bg-blue-800 text-gray-300 justify-self-center-safe">
+        <h1>Minimal Chess Engine</h1>
+        <h2>A simple to use chess website, no fluff, no stress.</h2>
+      </div>
+    </main>
   )
 }
