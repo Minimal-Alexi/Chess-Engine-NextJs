85bc4b5ae220658a25b210dfaed0ffc61ba6495a
Minimal-Alexi <alexpop@metropolia.fi>
2026-05-18 10:59:31 +0300

    Created async getMyGame function.

--- lib/games.ts
+++ lib/games.ts
@@ -5,3 +5,7 @@ const apiURL = process.env.API_URL;
 export async function getAllMyGames(){
     return authFetch(`${apiURL}/api/v1/games`);
 }
+
+export async function getMyGame(id:number){
+    return authFetch(`${apiURL}/api/v1/games/${id}`);
+}
