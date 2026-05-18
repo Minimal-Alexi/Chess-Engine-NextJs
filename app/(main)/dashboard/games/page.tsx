66ff99c8e4134996f09b17f434143dc59e99f13a
Minimal-Alexi <alexpop@metropolia.fi>
2026-05-18 08:40:40 +0300

    Successfully displayed users game.

--- app/(main)/dashboard/games/page.tsx
+++ app/(main)/dashboard/games/page.tsx
@@ -1,9 +1,23 @@
-const AllMyGamesPage = () => {
-    return (
-        <div>
-            <div>AllMyGamesPage</div>
-        </div>
-    )
-}
+import { Board } from "@/components/chess/Board";
+import { getAllMyGames } from "@/lib/games";
 
-export default AllMyGamesPage
+const AllMyGamesPage = async () => {
+  const res = await getAllMyGames();
+  const json = await res.json();
+  const games = json.games;
+  console.log(games);
+
+  return (
+    <div>
+      <div>AllMyGamesPage</div>
+
+      {games.map((game) => (
+        <div key={game.id}>
+          <Board map={game.state.board}/>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AllMyGamesPage;
