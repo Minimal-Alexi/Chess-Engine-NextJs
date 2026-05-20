d73b6e9407cf50f60699194ed99f6a024e15318a
Minimal-Alexi <alexpop@metropolia.fi>
2026-05-20 11:34:13 +0300

    Creating active board layout.

--- components/chess_interactive/InteractiveBoard.tsx
+++ components/chess_interactive/InteractiveBoard.tsx
@@ -2,11 +2,38 @@
 
 import { ActiveChessProvider } from "@/context/activeChessContext";
 import {Game} from "@/types/game"
+import { RenderSquare } from "../chess_static/Tile";
 
-export const InteractiveBoard = ({ game }: { game: Game }) => {
+export const InteractiveBoard = ({ game, size = 100 }: { game: Game, size:number}) => {
+      const boardSize = 8;
+
     return(
         <ActiveChessProvider initialGame={game}>
-            <div></div>
+            <div
+                  style={{
+                    display: "grid",
+                    gridTemplateColumns: `repeat(${boardSize}, ${size}px)`,
+                    width: "fit-content",
+                    border: "2px solid black",
+                  }}
+                >
+                  {Array.from({ length: boardSize }).map((_, row) =>
+                  Array.from({ length: boardSize }).map((_, col) => (
+                            <RenderSquare
+                              key={`${row}-${col}`}
+                              row={row}
+                              col={col}
+                              piece={game.state.board[row][col]}
+                              size={size}
+                            />
+                          ))
+                        )}
+                </div>
+            <div>
+              <h2>Controls</h2>
+              <p>Click to select a piece</p>
+              <p>Press C to deselect a piece</p>
+            </div>
         </ActiveChessProvider>
     )
 }
